21c245aea483cc179738296722bf6cb0018cb59c
DerColaTrinker <info@toolwheel.de>
2026-01-04 12:40:14 +0100

    Tests auf Started-Property umgestellt und neuen Job-Test ergänzt
    In diesem Commit wurde die Testlogik von Completed auf Started umgestellt. Zudem wurde ein neuer Test für die Ausführung eines Methoden-Jobs hinzugefügt, die Testmethoden angepasst und die Verwendung von [DebuggerHidden] teilweise entfernt. Die Änderungen verbessern die Nachvollziehbarkeit und Testabdeckung der Jobausführung.
    
    - Property Completed in TestInstanz entfernt und durch Started ersetzt
    - Methode TestJob_Success hinzugefügt, die Started auf true setzt
    - Neuen Test Test_Execute_Job für Methoden-Jobs ergänzt
    - Test_Execute_Job_Inline prüft jetzt Started statt Completed
    - Test_Execute_Job_With_Exception umbenannt und auf Started umgestellt
    - [DebuggerHidden] im neuen Test auskommentiert
    - Testlogik insgesamt auf Started-Property umgestellt

--- Source/Extensions/JobManager/ToolWheel.Extensions.JobManager/integration/DefaultJobStart.cs
+++ Source/Extensions/JobManager/ToolWheel.Extensions.JobManager/integration/DefaultJobStart.cs
@@ -18,7 +18,12 @@ namespace ToolWheel
     {
         public class TestInstanz
         {
-            public bool Completed { get; set; } = false;
+            public bool Started { get; set; } = false;
+
+            public void TestJob_Success()
+            {
+                Started = true;
+            }
         }
 
         private IServiceProvider CreateServiceProvider(Action<IJobManagerConfigurationBuilder>? configure = null)
@@ -32,14 +37,12 @@ namespace ToolWheel
 
 
         [Test]
-        [DebuggerHidden]
+        //[DebuggerHidden]
         public void Test_Execute_Job()
         {
-            var instance = new TestInstanz();
-
             var provider = CreateServiceProvider(configure => configure
                 .ConfigureJobs(jobs => jobs
-                    .AddMethod(() => instance.Completed = true, m => m.Id("1"))
+                    .AddMethod<TestInstanz>(m => m.TestJob_Success, m => m.Id("1"))
                 ));
 
             var jobService = provider.GetRequiredService<IJobService>();
@@ -52,20 +55,45 @@ namespace ToolWheel
 
             jobTaskService.WaitAllTasks();
 
-            Assert.That(instance.Completed, Is.True);
             Assert.That(jobTask, Is.Not.Null);
             Assert.That(jobTask.Status, Is.EqualTo(JobTaskStatus.Success));
         }
 
         [Test]
         [DebuggerHidden]
-        public void Test_Execute_Job_With_Exception()
+        public void Test_Execute_Job_Inline()
+        {
+            var instance = new TestInstanz();
+
+            var provider = CreateServiceProvider(configure => configure
+                .ConfigureJobs(jobs => jobs
+                    .AddMethod(() => instance.Started = true, m => m.Id("1"))
+                ));
+
+            var jobService = provider.GetRequiredService<IJobService>();
+            var jobTaskService = provider.GetRequiredService<IJobTaskService>();
+            var job = jobService.Find("1");
+
+            Assert.That(job, Is.Not.Null);
+
+            var jobTask = jobTaskService.Execute(job);
+
+            jobTaskService.WaitAllTasks();
+
+            Assert.That(instance.Started, Is.True);
+            Assert.That(jobTask, Is.Not.Null);
+            Assert.That(jobTask.Status, Is.EqualTo(JobTaskStatus.Success));
+        }
+
+        [Test]
+        [DebuggerHidden]
+        public void Test_Execute_Job_Inline_With_Exception()
         {
             var instance = default(TestInstanz);
 
             var provider = CreateServiceProvider(configure => configure
                 .ConfigureJobs(jobs => jobs
-                    .AddMethod(() => instance.Completed = true, m => m.Id("1"))
+                    .AddMethod(() => instance.Started = true, m => m.Id("1"))
                 ));
 
             var jobService = provider.GetRequiredService<IJobService>();
